c7ccc9902227379d4ea71a372e38cae3502d7dfc
derk78 <williamsdj503@gmail.com>
2025-01-22 17:03:59 -0700

    MAC address changer

--- mac_changer.sh
+++ mac_changer.sh
@@ -2,10 +2,12 @@
 new_address=$1
 interface=$2
 
+read -p "Please enter the MAC address: " new_address
+read -p "Please enter the network interface: " interface
+
 check_mac() {
     if [[ $new_address =~ ^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$ ]]; then 
         echo "Entered valid MAC address"
-        exit 0
     else 
         echo "Please enter valid MAC address"
         exit 1
@@ -14,26 +16,26 @@ check_mac() {
 
 check_root() {
     if [ "$EUID" -ne 0 ]; then 
-        echo "Please run at root"
-        exit 1
-    else 
-        exit 0
+        echo "Please run the script at the root"
+        exit 1 
     fi
 }
 
 check_interface() {
     if [ -d "/sys/class/net/$interface" ]; then
         echo "The network interface exists"
-        exit 0 
     else 
-        exit 1
+        echo "The network interface does not exist. Please enter a valid interface."
+        exit 1 
     fi
 }
 
-ip link set $interface down 
+check_root
+check_mac
+check_interface
 
-ip link set "$new_address" hw ether "$interface"
-
+echo "Changing the MAC address for the $interface interface to $new_address"
+ip link set "$interface" down 
 ip link set dev "$interface" address "$mac_address"
-
-ip link set dev "$interface" up
+ip link set "$interface" up
+echo "MAC address was succesfully changed to $new_address"
